43d11485c9cae8d9caf404f2732695f55adcd3c0
anjjcodes <anjalimani1216@gmail.com>
2026-04-07 23:18:56 +0530

    weekdays update

--- calendar/components/calendar/CalenderGrid.tsx
+++ calendar/components/calendar/CalenderGrid.tsx
@@ -9,8 +9,22 @@ export default function CalendarGrid(){
 
     const dates = generateData(year, month);
 
+    const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
     return (
         <div className="grid grid-cols-7 gap-2 mt-6">
+
+            {weekDays.map((day, index) => (
+                <div key={index}
+                className="h-10
+                flex
+                items-center
+                justify-center
+                rounded-lg
+                text-black">
+                    {day}
+                </div>
+            ))}
 
             {dates.map((date, index) => (
                 <div key={index}
